7a38934c5283b4bbb0aeffbfd754ec818b01c01d
shaiKemhi <shaikimhi221199@gmail.com>
2018-05-19 20:23:59 +0300

    app changes

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ import server
 
 def start(btn):
     port = app.getEntry("port")
-    if(port == None):
-        port = 8080
+    if(port == ''):
+        port = "8080"
     server.PORT_NUMBER = int(port)
     thread = Thread(target=server.run_server)
     thread.start()
+    app.stop()
     new_app(port)
     
+def stop_server(btn):
+    raise KeyboardInterrupt()
+#TODO : Add Image for Logo on app background
 def new_app(port):
     logo = gui("PSAP")
     logo.addLabel("l1","Server Running on: " + port)
+    logo.addButton("Stop Server", stop_server)
     logo.go()
 
 app = gui("PSAP")
-app.addButton("start", start)
+app.addButton("Start", start)
 app.addEntry("port")
 app.setEntryDefault("port","Enter Port Number Here")
 app.go()
